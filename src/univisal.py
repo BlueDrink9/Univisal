4d2b8b58000b0fad9739e2e81d725f8eaeaafcad
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-09-19 23:21:55 +1200

    Fix server: returns handled key

--- src/univisal.py
+++ src/univisal.py
@@ -4,35 +4,11 @@
 import socket
 from threading import Thread
 
-
-MAX_LENGTH = 4096
-
-def handle(clientsocket):
-  while 1:
-    buf = clientsocket.recv(MAX_LENGTH)
-    # if buf == '': return #client terminated connection
-    # We only handle one key at a time. Several characters is bad input.
-    if len(buf) != 1: return
-    # Todo: make enum.
-    if mode == "insert":
-        return buf
-    else:
-        handleKey(buf)
-
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 PORT = 10000
 HOST = '127.0.0.1'
 
-serversocket.bind((HOST, PORT))
-serversocket.listen(10)
-
-while 1:
-    #accept connections from outside
-    (clientsocket, address) = serversocket.accept()
-
-    ct = Thread(target=handle, args=(clientsocket,))
-    ct.start()
+global mode
+mode = 'test'
 
 def handleKey(key):
     if key == "m":
@@ -41,3 +17,29 @@ def handleKey(key):
         return "d"
     else:
         return key
+
+MAX_LENGTH = 4096
+def handle(clientsocket):
+  while 1:
+    buf = clientsocket.recv(MAX_LENGTH)
+    # if buf == '': return #client terminated connection
+    # We only handle one key at a time. Several characters is bad input.
+    if len(buf) != 1: return
+    # TODO: make modes enum.
+    if mode == "insert":
+        print(buf)
+    else:
+        output = handleKey(buf)
+        print(output)
+    return
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((HOST, PORT))
+serversocket.listen(10)
+
+while 1:
+    #accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+    ct = Thread(target=handle, args=(clientsocket,))
+    ct.start()
