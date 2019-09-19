7aa805c01f437baa2aa974f4317d34d91952dcef
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-09-19 23:23:44 +1200

    Comment thin client

--- src/univi.py
+++ src/univi.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-15 -*-
+# This is just a thin CLI tool to interact with univisal.py.
+# Usage: univi.py [key]
+# Takes only a single argument.
 import socket
 import sys
 
@@ -14,6 +17,7 @@ import sys
 # print "Number of arguments: ", len(sys.argv)
 # print "The arguments are: " , str(sys.argv)
 if len(sys.argv) != 2:
+    print("Usage: univi.py [key]")
     sys.exit(1)
 # print "The arguments are: " , str(sys.argv)
 s.send(str(sys.argv[1]))
