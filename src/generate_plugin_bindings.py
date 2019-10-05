21d0ae1962c78404a3b499544ec209072f6b0daa
bluedrink9 <bluedrink9@github.com>
2019-10-05 14:42:33 +1300

    Convert binding generator to take a stringformat

--- src/generate_plugin_bindings.py
+++ src/generate_plugin_bindings.py
@@ -4,12 +4,23 @@ import string
 import os
 import sys
 
-input_key_prefix=""
-input_key_suffix="::"
-run_command_prefix="univiResultFromKey(\""
-run_command_suffix="\")"
-fileext = ".ahk"
+# Hack to get python to store literal '%s' in string. "%ss% % '%'
+# d::univiResultFromKey("d")
+cmdformat_ahk = "%ss::univiResultFromKey(\"%ss\")" % ('%', '%')
+# d
+#   xdotool key $(univi_handleKey 'd')
+cmdformat_sxhkd = "%ss\n\txdotool key $(univi_handleKey %ss)" % ('%', '%')
 
+if len(sys.argv) != 2:
+    print("Usage: generate_plugin_bindings.py [string]")
+    print("[string] is a printf-formatted string. \
+          It is the command to generate for the plugin, \
+          with %ss in two places: Binding and send position" % ('%'))
+    sys.exit(1)
+
+cmdformat = str(sys.argv[1])
+# cmdformat = cmdformat_ahk
+cmdformat = cmdformat_sxhkd
 
 def mapPluginInputKey(key):
     return key
@@ -17,7 +28,7 @@ def mapPluginInputKey(key):
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
-f=open(get_script_path() + "/../plugins/bindings%s" % fileext, "w+")
+f=open(get_script_path() + "/../plugins/generated_bindings", "w+")
 
 # Standard input keys
 keys = string.ascii_letters + \
@@ -25,5 +36,6 @@ keys = string.ascii_letters + \
         "Esc" + \
         string.punctuation
 for key in keys:
-    f.write(input_key_prefix + mapPluginInputKey(key) + input_key_suffix + \
-            run_command_prefix + key + run_command_suffix + "\n")
+    # Doing a double escape, to expand the formatting stored in the variable.
+    f.write(f"{cmdformat}\n" % (key, key))
+f.close()
