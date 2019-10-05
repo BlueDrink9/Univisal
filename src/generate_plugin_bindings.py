2427983d3363ff31f52bad3f2ab3d73eb58d4b9b
bluedrink9 <bluedrink9@github.com>
2019-10-05 14:55:30 +1300

    Fix expanding escape chars in format string

--- src/generate_plugin_bindings.py
+++ src/generate_plugin_bindings.py
@@ -3,6 +3,8 @@
 import string
 import os
 import sys
+# For expand_escapes
+import codecs
 
 # Hack to get python to store literal '%s' in string. "%ss% % '%'
 # d::univiResultFromKey("d")
@@ -11,22 +13,23 @@ cmdformat_ahk = "%ss::univiResultFromKey(\"%ss\")" % ('%', '%')
 #   xdotool key $(univi_handleKey 'd')
 cmdformat_sxhkd = "%ss\n\txdotool key $(univi_handleKey %ss)" % ('%', '%')
 
+def expand_escapes(string):
+    return codecs.escape_decode(bytes(string, "utf-8"))[0].decode("utf-8")
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
 if len(sys.argv) != 2:
     print("Usage: generate_plugin_bindings.py [string]")
     print("[string] is a printf-formatted string. \
           It is the command to generate for the plugin, \
           with %ss in two places: Binding and send position" % ('%'))
+    print("There are examples in the source")
     sys.exit(1)
 
-cmdformat = str(sys.argv[1])
+cmdformat = expand_escapes(str(sys.argv[1]))
 # cmdformat = cmdformat_ahk
-cmdformat = cmdformat_sxhkd
-
-def mapPluginInputKey(key):
-    return key
-
-def get_script_path():
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
+# cmdformat = cmdformat_sxhkd
 
 f=open(get_script_path() + "/../plugins/generated_bindings", "w+")
 
@@ -37,5 +40,6 @@ keys = string.ascii_letters + \
         string.punctuation
 for key in keys:
     # Doing a double escape, to expand the formatting stored in the variable.
+    # May be easier to use the Template module though.
     f.write(f"{cmdformat}\n" % (key, key))
 f.close()
