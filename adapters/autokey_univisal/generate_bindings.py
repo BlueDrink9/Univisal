88eb53e4b4be1d9360239c092ef9d8359dd34a82
bluedrink9 <bluedrink9@github.com>
2019-10-06 18:33:03 +1300

    Add mappings for autokey

--- adapters/autokey_univisal/generate_bindings.py
+++ adapters/autokey_univisal/generate_bindings.py
@@ -1,56 +1,7 @@
 import string
 import json
-script_maps = { "alt": "<alt>",
-                "alt_gr": "<alt_gr>",
-                "backspace": "<backspace>",
-                "capslock": "<capslock>",
-                "ctrl": "<ctrl>",
-                "delete": "<delete>",
-                "up": "<up>",
-                "down": "<down>",
-                "right": "<right>",
-                "left": "<left>",
-                "return": "<enter>",
-                "esc": "<escape>",
-                # "f1-f12": "<f1>-\<f12>",
-                "home": "<home>",
-                "end": "<end>",
-                "insert": "<insert>",
-                "menu": "<menu>",
-                "np_add": "<np_add>",
-                "np_delete": "<np_delete>",
-                "np_divide": "<np_divide>",
-                "np_down": "<np_down>",
-                "np_up": "<np_up>",
-                "np_left": "<np_left>",
-                "np_right": "<np_right>",
-                "np_end": "<np_end>",
-                "np_home": "<np_home>",
-                "np_insert": "<np_insert>",
-                "np_multiply": "<np_multiply>",
-                "np_page_down": "<np_page_down>",
-                "np_page_up": "<np_page_up>",
-                "np_subtract": "<np_subtract>",
-                "numlock": "<numlock>",
-                "page_down": "<page_down>",
-                "page_up": "<page_up>",
-                "pause": "<pause>",
-                "print_screen": "<print_screen>",
-                "scroll_lock": "<scroll_lock>",
-                "shift": "<shift>",
-                # "space": "(space character)",
-                "super": "<super>",
-                "tab": "<tab>"
-                }
-
-def get_script_path():
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
-
-def mapPluginInputKey(key):
-    if key in script_maps:
-        return script_maps[key]
-    else:
-        return key
+from ../../src/adapter_maps import *
+from ../../src/library import *
 
 keys = list(string.ascii_letters + \
     string.digits + \
@@ -67,7 +18,7 @@ for key in keys:
             modifiers.append("<shift>")
             description = "shift"+key
     # Use lowercase for hotkey, with modifiers.
-    hotkey = mapPluginInputKey(key.lower())
+    hotkey = getAdapterMap(key.lower())
     phrase = "<script name=univi args={}>".format(key)
     # Actually creates a phrase, apparently.
     # engine.create_hotkey(folder, description, modifiers, key, contents)
