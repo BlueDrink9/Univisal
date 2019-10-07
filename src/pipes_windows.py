938e288a80cf4b6fbffd491bd18adec48b3d8f02
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-10-07 19:00:41 +1300

    Write functs for pipes_windows

--- src/pipes_windows.py
+++ src/pipes_windows.py
@@ -6,7 +6,9 @@ import pywintypes
 def makeWin32Pipe(name):
     # https://docs.microsoft.com/en-us/windows/win32/ipc/named-pipes
     openMode = win32pipe.PIPE_ACCESS_DUPLEX
-    pipeMode = win32pipe.PIPE_TYPE_MESSAGE  | win32pipe.PIPE_WAIT
+    # write/read messages, not bytes. Blocking.
+    pipeMode = win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE \
+            | win32pipe.PIPE_WAIT
     sa = pywintypes.SECURITY_ATTRIBUTES()
     sa.SetSecurityDescriptorDacl ( 1, None, 0 )
     pipe_handle = win32pipe.CreateNamedPipe(name,
@@ -17,9 +19,26 @@ def makeWin32Pipe(name):
                                             0,
                                             2000,
                                             sa)
+    return pipe_handle
 
 
 def makePipes():
-    readpipe = makeWin32Pipe(r'\\.\pipe\univisal.in.fifo')
-    writepipe = makeWin32Pipe(r'\\.\pipe\univisal.out.fifo')
+    readpipe = r'\\.\pipe\univisal.in.fifo'
+    writepipe = r'\\.\pipe\univisal.out.fifo'
+    makeWin32Pipe(readpipe)
+    makeWin32Pipe(writepipe)
     return readpipe, writepipe
+
+
+def readPipe(handle):
+    bufSize = 2000
+    timeout = 1
+    out = win32pipe.CallNamedPipe(handle, bytes("", "UTF-8"), bufSize, timeout)
+    return out
+
+
+def writePipe(handle, message):
+    bufSize = 0
+    timeout = 1
+    out = win32pipe.CallNamedPipe(handle, message, bufSize, timeout)
+    return out
