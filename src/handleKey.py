2e770d89e97289b9f31ea16ece4afff8772ee609
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-10-31 15:54:09 +1300

    Fix 'a'

--- src/handleKey.py
+++ src/handleKey.py
@@ -10,10 +10,12 @@ from adapter_maps import getAdapterMap
 
 
 def handleKey(key):
+    # Reduce chance of a typo if returning nop
+    nop = "nop"
     if key.lower() == "esc":
         setMode(Mode.normal)
         # No op. Need to send something back to signal finish.
-        return "nop"
+        return nop
     if getMode() == Mode.insert:
         return key
     elif key == "h":
@@ -30,11 +32,10 @@ def handleKey(key):
         return getAdapterMap(Motion.goLineEnd.name)
     elif key == "i":
         setMode(Mode.insert)
-        return "nop"
+        return nop
     elif key == "a":
-        # TODO: Add motion
         setMode(Mode.insert)
-        return "nop"
+        return getAdapterMap(Motion.goRight.name)
     elif key == "I":
         setMode(Mode.insert)
         return getAdapterMap(Motion.goLineStart.name)
