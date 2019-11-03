54b174d12427983d4f7ba0dfba539831cba847a0
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-11-03 23:14:45 +1300

    Fix pipename param

--- src/univisal/pipes_windows.py
+++ src/univisal/pipes_windows.py
@@ -33,10 +33,10 @@ def readPipe(pipename="univisal.in.fifo"):
             pass
 
 
-def makeWritePipe(pipename="univisal.out.fifo"):
+def makeWritePipe(pipename):
     readpipeName = r'\\.\pipe\\' + pipename
     writepipeh = win32pipe.CreateNamedPipe(
-        name,
+        readpipeName,
         win32pipe.PIPE_ACCESS_DUPLEX,
         win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
         1, 65536, 65536,
@@ -45,12 +45,12 @@ def makeWritePipe(pipename="univisal.out.fifo"):
     return writepipeh
 
 
-def writePipe(msg):
+def writePipe(msg, pipename="univisal.out.fifo"):
     # Have to close pipe to finish sending message, meaning you have to re-open
     # it here.
     # There may be an alternative to having to totally re-open the pipe. Look
     # for named pipe code loops that don't fully close it.
-    pipe = makeWritePipe()
+    pipe = makeWritePipe(pipename)
     try:
         logger.info("Waiting for pipe to be read")
         win32pipe.ConnectNamedPipe(pipe, None)
