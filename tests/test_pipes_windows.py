f62160d4af5d71ca8c9df223afd974cd78ec51bd
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-11-04 11:38:52 +1300

    Refactor round_trip_msg

--- tests/test_pipes_windows.py
+++ tests/test_pipes_windows.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-import sys, os
+import os
 import pytest
+import sys
+import threading
 
 # Add src dir to the python path so we can import.
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
@@ -9,20 +11,24 @@ from univisal.pipes_windows import readPipe, writePipe
 # TODO: Thread this so that messages can be read after writing, without
 # waiting for connection to finish.
 # (Probably try reading first to get fastest results?)
-
-def test_single_char():
-    pipe = "pipe_test"
-    msg = "t"
+def round_trip_msg(msg, pipe=None):
+    if pipe is None:
+        # TODO make this a unique generation to prevent 1 broken test holding
+        # up another one.
+        pipe = "pipe_test"
     writePipe(msg, pipe)
     result = readPipe(pipe)
+    return result
+
+def test_single_char():
+    msg = "t"
+    result = round_trip_msg(msg)
     assert result == msg, "A single character"
 
 
 def test_multi_char():
-    pipe = "pipe_test"
     msg = "test"
-    writePipe(msg, pipe)
-    result = readPipe(pipe)
+    result = round_trip_msg(msg)
     assert result == msg, "Multiple characters through the pipe"
 
 
@@ -30,8 +36,7 @@ def test_multi_msg():
     pipe = "pipe_test"
     for m in range(1, 10):
         msg = "test" + str(m)
-        writePipe(msg, pipe)
-        result = readPipe(pipe)
+        result = round_trip_msg(msg, pipe)
         assert result == msg, \
             "Many characters, multiple times quickly through the pipe"
 
@@ -40,8 +45,7 @@ def test_pipe_latency():
     pipe = "pipe_test"
     for m in range(1, 10):
         msg = "test" + str(m)
-        writePipe(msg, pipe)
-        result = readPipe(pipe)
+        result = round_trip_msg(msg, pipe)
         assert result == msg, \
             "Many characters, multiple times quickly through the pipe"
         # TODO: time at start and end of writing and reading, make sure it is
