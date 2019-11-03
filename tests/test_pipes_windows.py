8107347e115ae939d82d885dfc6d937cd7bc7f72
bluedrink9 <bluedrink9@no-reply.users.github.com>
2019-11-03 22:55:55 +1300

    Remove tests init.py based on pytest recommendation

--- tests/test_pipes_windows.py
+++ tests/test_pipes_windows.py
@@ -3,12 +3,11 @@ import sys, os
 import pytest
 
 # Add src dir to the python path so we can import.
-sys.path.append(os.path.abspath(os.path.join('src')))
-# target = __import__("src.pipes_windows.py")
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 
 # import ..src.pipes_windows as target
 # from ..src.pipes_windows import readPipe, writePipe
-from pipes_windows import readPipe, writePipe
+from univisal.pipes_windows import readPipe, writePipe
 # readPipe = target.readPipe
 # writePipe = target.writePipe
 
