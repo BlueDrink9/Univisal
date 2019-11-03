2ea4dba507a6198e44d21b54acab4544b9f1039f
bluedrink9 <bluedrink9@users.noreply.github.com>
2019-11-04 11:58:33 +1300

    Add test for latency

--- tests/test_pipes_windows.py
+++ tests/test_pipes_windows.py
@@ -3,6 +3,7 @@ import os
 import pytest
 import sys
 import threading
+import time
 
 # Add src dir to the python path so we can import.
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
@@ -18,6 +19,7 @@ def round_trip_msg(msg, pipe=None):
         pipe = "pipe_test"
     x = threading.Thread(target=writePipe, args=(msg, pipe))
     x.start()
+    time.sleep(0.01)
     # writePipe(msg, pipe)
     result = readPipe(pipe)
     return result
@@ -37,7 +39,7 @@ def test_multi_char():
 
 def test_multi_msg():
     pipe = "pipe_test"
-    for m in range(1, 10):
+    for m in range(1, 30):
         msg = "test" + str(m)
         result = round_trip_msg(msg, pipe)
         assert result == msg, \
@@ -46,16 +48,22 @@ def test_multi_msg():
 
 def test_pipe_latency():
     pipe = "pipe_test"
-    for m in range(1, 10):
+    pretime = time.time()
+    n = 30
+    for m in range(1, n):
         msg = "test" + str(m)
+        trippretime = time.time()
         result = round_trip_msg(msg, pipe)
+        tripposttime = time.time()
         assert result == msg, \
             "Many characters, multiple times quickly through the pipe"
-        # TODO: time at start and end of writing and reading, make sure it is
-        # very small.
-        assert False
-    # TODO: Overall time should be very low
-    assert False
+        # Make sure round trip takes less than 0.011 ms (0.01 is a sleep in the
+        # trip anyway)
+        assert tripposttime - trippretime < 0.012
+    posttime = time.time()
+    # Make sure total trip takes less than 0.01 ms per round plus a few extra
+    # ms
+    assert posttime - pretime < 0.01 * n + 0.10
 
 
 if __name__ == '__main__':
