c2668c0dba18169c850fa2c6eecef8e2e0a7137e
bluedrink9 <bluedrink9@github.com>
2019-11-24 16:20:17 +1300

    Catch enum error

--- src/univisal/handleKey.py
+++ src/univisal/handleKey.py
@@ -19,41 +19,45 @@ logger = logging.getLogger(__name__)
 
 
 def handleKey(key):
-    # Reduce chance of a typo if returning nop
-    nop = "nop"
-    if key.lower() == "esc":
-        setMode(Mode.normal)
-        # No op. Need to send something back to signal finish.
-        return nop
-    if getMode() == Mode.insert:
-        return getAdapterMap(key)
-    elif key == "h":
-        return getAdapterMap(Motion.left.name)
-    elif key == "l":
-        return getAdapterMap(Motion.right.name)
-    elif key == "j":
-        return getAdapterMap(Motion.down.name)
-    elif key == "k":
-        return getAdapterMap(Motion.up.name)
-    if key == "0":
-        return getAdapterMap(Motion.goLineStart.name)
-    if key == "$":
-        return getAdapterMap(Motion.goLineEnd.name)
-    elif key == "i":
-        setMode(Mode.insert)
-        return nop
-    elif key == "a":
-        setMode(Mode.insert)
-        return getAdapterMap(Motion.right.name)
-    elif key == "I":
-        setMode(Mode.insert)
-        return getAdapterMap(Motion.goLineStart.name)
-    elif key == "A":
-        setMode(Mode.insert)
-        return getAdapterMap(Motion.goLineEnd.name)
-    elif key == "w":
-        return getAdapterMap(Motion.goWordNext.name)
-    elif key == "b":
-        return getAdapterMap(Motion.goWordPrevious.name)
-    else:
-        return getAdapterMap(key)
+    try:
+        # Reduce chance of a typo if returning nop
+        nop = "nop"
+        if key.lower() == "esc":
+            setMode(Mode.normal)
+            # No op. Need to send something back to signal finish.
+            return nop
+        if getMode() == Mode.insert:
+            return getAdapterMap(key)
+        elif key == "h":
+            return getAdapterMap(Motion.left.name)
+        elif key == "l":
+            return getAdapterMap(Motion.right.name)
+        elif key == "j":
+            return getAdapterMap(Motion.down.name)
+        elif key == "k":
+            return getAdapterMap(Motion.up.name)
+        if key == "0":
+            return getAdapterMap(Motion.goLineStart.name)
+        if key == "$":
+            return getAdapterMap(Motion.goLineEnd.name)
+        elif key == "i":
+            setMode(Mode.insert)
+            return nop
+        elif key == "a":
+            setMode(Mode.insert)
+            return getAdapterMap(Motion.right.name)
+        elif key == "I":
+            setMode(Mode.insert)
+            return getAdapterMap(Motion.goLineStart.name)
+        elif key == "A":
+            setMode(Mode.insert)
+            return getAdapterMap(Motion.goLineEnd.name)
+        elif key == "w":
+            return getAdapterMap(Motion.goWordNext.name)
+        elif key == "b":
+            return getAdapterMap(Motion.goWordPrevious.name)
+        else:
+            return getAdapterMap(key)
+    except AttributeError:
+        logger.error("Incorrect enum specified when handling key {}".format(key), exc_info=True)
+        return key
