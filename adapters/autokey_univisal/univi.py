fd8a58b294fb9ff4004049cd71f157f15860ac58
bluedrink9 <bluedrink9@github.com>
2019-11-28 16:28:56 +1300

    univi.py (autokey): Convert to thin wrapper around univi.sh

--- adapters/autokey_univisal/univi.py
+++ adapters/autokey_univisal/univi.py
@@ -1,10 +1,9 @@
-from tempfile import gettempdir
-from multiprocessing import Process as Process_mp
-# from os import open as os_open
-# from os import O_RDONLY, O_NONBLOCK
+import os
+import subprocess
+import sys
 
-readpipe = gettempdir() + '/univisal.out.fifo'
-writepipe = gettempdir() + '/univisal.in.fifo'
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 key = engine.get_macro_arguments()[0]
 if key is None:
@@ -12,27 +11,10 @@ if key is None:
     exit(1)
 
 def univi():
-    # Opening pipes blocks until the other end is connected to.
-    outpt = open(writepipe, "w")
-    outpt.write(key)
-    outpt.close()
-    outpt = open(readpipe, "r")
-    # outpt = os_open(readpipe, O_RDONLY|O_NONBLOCK)
-    output = outpt.read()
-    outpt.close()
-    if output != "nop":
-        engine.set_return_value(output)
+    univi_sh = os.path.join(get_script_path(), "..", "..", "src", "univi.sh")
+
+    output = subprocess.check_output(["bash", univi_sh, key]).decode()
+    engine.set_return_value(output)
+
 
 univi()
-
-# exit(1)
-# # Start univi as a process
-# p = Process_mp(target=univi)
-# p.start()
-# # Wait for n seconds or until process finishes
-# p.join(0.5)
-# if p.is_alive():
-#     print("Univi timed out. Is univisal reading from {}?".format(readpipe))
-#     engine.set_return_value(key)
-#     p.terminate()
-#     p.join()
