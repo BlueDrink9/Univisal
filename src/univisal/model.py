ac04f67bd014ff749f94f1d5d9daf49faa249c1c
bluedrink9 <bluedrink9@github.com>
2020-01-12 00:41:50 +1300

    Update model warnings and interface

--- src/univisal/model.py
+++ src/univisal/model.py
@@ -1,6 +1,12 @@
 # enum from py >= 3.4
 from enum import Enum, auto
 import string
+import logging
+try:
+    from . import logging_
+except ImportError:
+    import logging_
+logger = logging.getLogger(__name__)
 
 # Not using terms "edit" or "Ex" because they are less familiar.
 class Mode(Enum):
@@ -12,10 +18,11 @@ class Mode(Enum):
     operator_pending  = auto()
 
 _current_mode = None
+_registers = None
+_search_letter = None
+_pending_motion = None
+_captured_clipboard = None
 pending_clipboard = None
-pending_motion = None
-captured_clipboard = None
-registers = None
 repeat_count = None
 
 insertlike_modes = [
@@ -37,8 +44,8 @@ def isMode(m):
 
 # Declare globals within a function to access them.
 def init_model():
-    global repeat_count, registers
-    global pending_clipboard, pending_motion, captured_clipboard
+    global repeat_count, _registers
+    global pending_clipboard, _pending_motion, _captured_clipboard
     setMode(Mode.normal)
     registers = {}
     for l in string.ascii_letters:
@@ -49,13 +56,29 @@ def init_model():
     captured_clipboard = None
 
 def getCapturedClipboard():
-    global captured_clipboard
-    out = captured_clipboard
-    captured_clipboard = None
+    global _captured_clipboard
+    if _captured_clipboard is None:
+        if pending_clipboard:
+            logger.error("Pending clipboard, but none was given \
+                        (captured_clipboard is blank).")
+        logger.warning("Clipboard was requested, but is None. Returning '' instead.")
+        return ''
+    out = _captured_clipboard
+    _captured_clipboard = None
     return out
 
 def getPendingMotion():
-    global pending_motion
-    out = pending_motion
-    pending_motion = None
+    global _pending_motion
+    out = _pending_motion
+    _pending_motion = None
     return out
+
+def getSearchLetter():
+    if not isinstance(_search_letter, str):
+        logger.warning("model._search_letter is not a str. \
+                Is '{}', of type {}".format(_search_letter, type(_search_letter)))
+    return _search_letter
+
+def setSearchLetter(l):
+    global _search_letter
+    _search_letter = l
