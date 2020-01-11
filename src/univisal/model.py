17f3e76359a176a5afcdf86acba3c24a10acab14
bluedrink9 <bluedrink9@github.com>
2020-01-11 23:51:16 +1300

    Init pendings better

--- src/univisal/model.py
+++ src/univisal/model.py
@@ -12,8 +12,11 @@ class Mode(Enum):
     operator_pending  = auto()
 
 _current_mode = None
-pending_clipboard = False
+pending_clipboard = None
 pending_motion = None
+captured_clipboard = None
+registers = None
+repeat_count = None
 
 insertlike_modes = [
         Mode.insert,
@@ -34,16 +37,25 @@ def isMode(m):
 
 # Declare globals within a function to access them.
 def init_model():
-    global repeat_count
-    global registers
+    global repeat_count, registers
+    global pending_clipboard, pending_motion, captured_clipboard
     setMode(Mode.normal)
     registers = {}
     for l in string.ascii_letters:
         registers[l] = ""
     repeat_count = 1
+    pending_clipboard = False
+    pending_motion = None
+    captured_clipboard = None
 
 def getCapturedClipboard():
     global captured_clipboard
     out = captured_clipboard
     captured_clipboard = None
+    return out
+
+def getPendingMotion():
+    global pending_motion
+    out = pending_motion
+    pending_motion = None
     return out
