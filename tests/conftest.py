a21313f1aab7977d5de6161eade5403502497b22
bluedrink9 <bluedrink9@github.com>
2020-01-16 15:55:21 +1300

    Move conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,13 +5,7 @@ import pytest
 @pytest.fixture(autouse=True)
 def no_log_errors(caplog):
     yield  # Run in teardown
-    print('fixture loaded!')
-    expect(False)
-    assert(False)
+    print(caplog.records)
     caplog.set_level(logging.INFO)
     errors = [record for record in caplog.records if record.levelno >= logging.ERROR]
-    if errors:
-        pytest.fail()
-    pytest.fail()
-    expect(not errors)
     assert not errors
