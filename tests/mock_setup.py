5d8c6e923259ded132818a320d6cd9cdd5fdede3
bluedrink9 <bluedrink9@github.com>
2020-01-16 15:37:07 +1300

    Fix setup mock

--- tests/mock_setup.py
+++ tests/mock_setup.py
@@ -19,23 +19,28 @@ def ret_arg(arg):
     return arg
 
 
+def ret_joinChar():
+    return "+"
+
+
 def init_univisal(with_interface=False):
     mockargs=['1', '2']
-    if not with_interface:
-        interfaceInit = "univisal.message_interface.init_message_interface"
-        interfacePatch = unittest.mock.patch(interfaceInit, create=True)
-    else:
-        interfacePatch=None
+    interfaceInit = "univisal.message_interface.init_message_interface"
     with unittest.mock.patch('sys.argv', mockargs), \
-    interfacePatch, \
-    unittest.mock.patch("univisal.adapter_maps.load_adapter_maps",
-            side_effect=mock_adapter_maps), \
-    unittest.mock.patch("univisal.adapter_maps.getAdapterMap",
-        side_effect=ret_arg), \
-    unittest.mock.patch("univisal.adapter_maps.getJoinChar",
-            side_effect=ret_arg):
-        from univisal.univisal import main
-        main()
+            unittest.mock.patch("univisal.adapter_maps.load_adapter_maps",
+                                side_effect=mock_adapter_maps), \
+            unittest.mock.patch("univisal.adapter_maps.getAdapterMap",
+                                side_effect=ret_arg), \
+            unittest.mock.patch("univisal.adapter_maps.getJoinChar",
+                                side_effect=ret_joinChar):
+        if with_interface:
+            from univisal.univisal import main
+            main()
+            univisal.message_interface.init_message_interface()
+        else:
+            with unittest.mock.patch(interfaceInit, create=True):
+                from univisal.univisal import main
+                main()
     univisal.config.config = {}
     clear_maps()
 
