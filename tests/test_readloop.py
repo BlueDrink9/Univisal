28994a25a216d9bba5714c92ff8e9747d1146304
bluedrink9 <bluedrink9@github.com>
2020-01-16 15:54:28 +1300

    Add test for main loop

---
+++ tests/test_readloop.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import os
+import pytest
+import unittest.mock
+import sys
+import logging
+# Add src dir to the python path so we can import.
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+import univisal
+from univisal.keys import Keys
+from univisal.motion import Motion
+from tests.mock_setup import init_univisal
+
+# @pytest.fixture(autouse=True)
+# def setUp():
+#     init_univisal(with_interface=True)
+
+outpts=[]
+outI=-1
+def addOutput(key):
+    print(key)
+    global outpts
+    outpts.append(key)
+inpts=[]
+inI=-1
+def setInputs(keys):
+    global inpts
+    inpts=keys
+
+def mock_write(key):
+    addOutput(key)
+def mock_read():
+    global inI
+    inI += 1
+    return inpts[inI]
+
+
+def init_loop():
+    # caplog.set_level(logging.DEBUG)
+    with unittest.mock.patch("univisal.message_interface.outpt_write",
+                     side_effect=mock_write), \
+            unittest.mock.patch("univisal.message_interface.inpt_read",
+                                side_effect=mock_read):
+        init_univisal(with_interface=True)
+
+def loop(keys):
+    # Copy to stop HUP ending up in failure output.
+    send = keys.copy()
+    # Stop the loop at the end of the input keys.
+    send.append("HUP")
+    setInputs(send)
+    init_loop()
+
+
+@pytest.mark.parametrize("keys, expected", [
+    (["l"], Motion.right.value),
+    (["l", 'l'], Motion.right.value * 2),
+    ])
+def test_loop(keys, expected):
+    if not isinstance(keys, list):
+        keys = [keys]
+    if not isinstance(expected, list):
+        expected = [expected]
+    loop(keys)
+    assert outpts==expected, "Main loop failed with keys {}".format(keys)
