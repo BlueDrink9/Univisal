74b9e64ee4089c0cb72ed76196995a65624220c0
bluedrink9 <bluedrink9@github.com>
2020-01-17 14:41:52 +1300

    Update test

--- tests/test_model.py
+++ tests/test_model.py
@@ -12,11 +12,11 @@ from univisal.model import increaseRepeatCount
 
 @pytest.mark.parametrize("numbers", [1, 10, 15, 329, 991])
 def test_increaseRepeatCount(numbers):
-    model.repeat_count = 0
+    model.clear_pending()
     # Only inputs one digit at a time, so this iterates them.
+    # Using as a string treats numbers like the individual digits.
     for number in str(numbers):
         increaseRepeatCount(int(number))
-    assert model.repeat_count == numbers, \
+    # This time using numbers as int, as all digits.
+    assert model.getRepeatCount() == numbers, \
         "Repeat count is incorrect after {}".format(numbers)
-
-
