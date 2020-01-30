34aa6539220f984f3343d8d92b49bcd5025a1b95
bluedrink9 <bluedrink9@github.com>
2020-01-30 16:59:12 +1300

    Populate setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
 setup(
     name = 'univisal',
+    version="0.0.1",
+    author="bluedrink9",
+    description="universal vi emulation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages = find_packages(),
+    url="https://github.com/bluedrink9/univisal",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
 )
+
