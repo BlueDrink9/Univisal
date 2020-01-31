4e3f329a8a3efeecff5daa1e0504705fbfa29443
bluedrink9 <bluedrink9@github.com>
2020-01-31 18:56:11 +1300

    Refactor remap algorithm

--- src/univisal/remap.py
+++ src/univisal/remap.py
@@ -87,35 +87,47 @@ def resolve_map(key):
     # to wait to see what next key is. If it isn't part of a map, expand. But
     # what about timing? What if there is no next key? Probably best to not
     # allow maps inside maps.
-    expired_maps = []
-    for map_ in maps_in_progress:
-        index = maps_in_progress[map_]
-        # logger.debug("map index {}; map[index] {}; key {}".format(
-        #     index, map_[index], key))
-        if map_[index] == key:
-            logger.debug("Key '{}' is member {} of mapped sequence '{}'".format(key, index+1, map_))
-            maps_in_progress[map_] += 1
-        else:
-            # Broken sequence
-            expired_maps.append(map_)
-        if maps_in_progress[map_] == len(map_):
-            # Full sequence has been matched
-            maps_in_progress = {}
-            backspaces = []
-            if modeIsInsertLike():
-                # return appropriate number of backspaces.
-                backspaces = ["<bs>"] * (len(map_) - 1)
-            remappedKeys = backspaces + [current_maps[map_]]
-            logger.info("Expanded map '{}' as '{}'".format(map_,
-                                                           remappedKeys))
-            return remappedKeys
-    for k in expired_maps:
-        del maps_in_progress[k]
+    maps_no_longer_in_progress = []
+    for map_, progress in maps_in_progress.items():
+        thisKeyIsNextInMap = map_[progress] == key
+
+        if thisKeyIsNextInMap:
+            incrementMapProgress(map_)
+            logger.debug("Key '{}' is member {} of mapped sequence '{}'".format(key, progress+1, map_))
+        else:  # Broken sequence
+            maps_no_longer_in_progress.append(map_)
+
+        if isCompleteMatch(map_):
+            resetMapsInProgress()
+            return getRemappedKeys(map_)
+
+    for m in maps_no_longer_in_progress:
+        del maps_in_progress[m]
     return [key]
+
+def getEnoughBackspacesToErase(map_):
+    backspaces = []
+    if modeIsInsertLike():
+        # return appropriate number of backspaces.
+        backspaces = ["<bs>"] * (len(map_) - 1)
+    return backspaces
+
+def getRemappedKeys(map_):
+    backspaces = getEnoughBackspacesToErase(map_)
+    remappedKeys = backspaces + [current_maps[map_]]
+    logger.info("Expanded map '{}' as '{}'".format(map_,
+                                                   remappedKeys))
+    return remappedKeys
+
+def isCompleteMatch(map_):
+    return maps_in_progress[map_] == len(map_)
+
+def incrementMapProgress(map_):
+    maps_in_progress[map_] += 1
 
 
 def resetMapData():
-    global imaps, nmaps, vmaps, cmaps, current_mode, current_maps, current_sequence, maps_in_progress
+    global imaps, nmaps, vmaps, cmaps, current_mode, current_maps
     imaps = {}
     nmaps = {}
     vmaps = {}
@@ -123,5 +135,9 @@ def resetMapData():
 
     current_mode = None
     current_maps = None
+    resetMapsInProgress()
+
+def resetMapsInProgress():
+    global current_sequence, maps_in_progress
     current_sequence = []
     maps_in_progress = {}
