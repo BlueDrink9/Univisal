e7fd50fc3e732f933ae7f2ea866bb0267ccfeeb4
bluedrink9 <bluedrink9@github.com>
2020-02-02 13:50:08 +1300

    Refactor handleInput

--- src/univisal/handleInput.py
+++ src/univisal/handleInput.py
@@ -6,6 +6,7 @@ try:
     from . import command
     from .handleKey import handleVimInputKey
     from .adapter_maps import getAdapterMap
+    from .keys import Keys
 except ImportError:
     from library import *
     from model import Mode, getMode, setMode, isMode
@@ -14,36 +15,45 @@ except ImportError:
     import command
     from handleKey import handleVimInputKey
     from adapter_maps import getAdapterMap
+    from keys import Keys
 logger = __import__("univisal.logger").logger.get_logger(__name__)
 
 def handleInput(input_):
     logger.debug("handleInput input_: {}".format(input_))
     try:
-        # nop = No operation. Need to send something back to adapter to signal finish.
-        # Reduce chance of a typo if returning nop
-        nop = "nop"
-
-        # For specific commands sent from adapter, e.g. `:disable`.
-        # These should be handled specially, before other logic.
-        if len(input_) > 1 and input_[0] == ":":
-            commandResult = command.handle(input_)
-            if commandResult is None:
-                return nop
-            else:
-                return commandResult
-            return
-        # Disabled: always return input key.
-        if isMode(Mode.disabled):
-            return input_
-
-        if model.expecting_search_letter:
-            model.setSearchLetter(input_)
-            normalCommand(model.pending_motion)
-            return model.popOutputKeys()
-
-        return handleVimInputKey(input_)
+        return handleInputUnsafe(input_)
     except:
         return getFallbackOutput(input_)
+
+def handleInputUnsafe(input_):
+    if len(input_) > 1 and input_[0] == ":":
+        # For specific commands sent from adapter, e.g. `:disable`.
+        # These should be handled specially, before other logic.
+        return handleUnivisalCommand(input_)
+
+    # Disabled: always return input key.
+    if isMode(Mode.disabled):
+        return input_
+
+    if model.expecting_search_letter:
+        return handleExpectedSearchLetter(input_)
+
+    # The default responce
+    return handleVimInputKey(input_)
+
+def handleUnivisalCommand(input_):
+    # For specific commands sent from adapter, e.g. `:disable`.
+    # These should be handled specially, before other logic.
+    commandResult = command.handle(input_)
+    if commandResult is None:
+        return Keys.nop
+    else:
+        return commandResult
+
+def handleExpectedSearchLetter(input_):
+    model.setSearchLetter(input_)
+    normalCommand(model.pending_motion)
+    return model.popOutputKeys()
 
 def getFallbackOutput(input_):
     logger.critical("Unhandled exception handling input", exc_info=True)
