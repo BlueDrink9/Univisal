20496c8b7960194e979b7cefe68d205d914b3691
bluedrink9 <bluedrink9@github.com>
2020-02-02 13:09:48 +1300

    Test loadAdapterMaps

--- tests/test_adapter_maps.py
+++ tests/test_adapter_maps.py
@@ -7,25 +7,32 @@ from univisal.keys import Keys
 from univisal import adapter_maps
 
 
-@pytest.fixture(scope="function", autouse=True)
-def setup(caplog, tmpdir):
-    adapter_maps.adapter_maps = {}
-    # caplog.set_level(logging.DEBUG)
-    # with unittest.mock.patch('univisal.config.getConfigPath',
-    #                          return_value=tmpdir / "univisal" / "config.json"):
-    #     init_config()
-
 def dumpDictToFile(dict_, path):
     with open(path, 'w') as outfile:
         json.dump(dict_, outfile, indent=2, ensure_ascii=False)
 
-@pytest.fixture(scope='function', autouse=True)
-def mapPath(tmpdir):
-    with unittest.mock.patch(
+
+@pytest.fixture(scope="function", autouse=True)
+def setup(caplog, tmpdir):
+    adapter_maps.adapter_maps = {}
+
+@pytest.fixture(scope='session', autouse=True)
+def mapPath(tmpdir_factory):
+    """
+    :type request: _pytest.python.SubRequest
+   :return:
+    """
+    tmpdir = tmpdir_factory.mktemp("mappings")
+    patched = unittest.mock.patch(
         'univisal.adapter_maps.getMappingPath',
-        return_value=tmpdir / "univisal" / "mappings.json"
-    ) as path:
-        yield path
+        return_value=tmpdir / "mappings.json"
+    )
+    patched.__enter__()
+
+    def unpatch():
+        patched.__exit__()
+        log.info("Patching complete. Unpatching")
+        request.addfinalizer(unpatch)
 
 @pytest.mark.parametrize("mock_maps, key, expected, error_msg", [
     ({Keys.esc.value: "escape"}, Keys.esc.value, "escape",
@@ -51,117 +58,17 @@ def test_getJoinChar(caplog, mock_maps, key, expected, error_msg):
     adapter_maps.adapter_maps = mock_maps
     assert adapter_maps.getJoinChar() == expected, error_msg
 
+# def test_getMappingPath():
 
-# @pytest.mark.parametrize("test_opt, expected, error_msg", [
-#     ("imaps", {}, "loads dict option incorrectly"),
-# ])
-# def test_load_adapter_maps(caplog, tmpdir, test_opt, expected, error_msg):
-#     caplog.set_level(logging.DEBUG)
-#     with unittest.mock.patch('univisal.config.getConfigPath',
-#                              return_value=tmpdir / "univisal" / "config.json"):
-#         makeDefaults()
-#         loadConfig()
-#     assert getConfigOption(test_opt) == expected, error_msg
+def test_loadAdapterMaps(mapPath):
+    key = Keys.space.value
+    value = "{space}"
+    mock_maps = {key: value}
+    dumpDictToFile(mock_maps, adapter_maps.getMappingPath(""))
 
-# @pytest.mark.parametrize("conf, test_opt, expected, error_msg", [
-#     ({"imaps": {"jk": "<esc>"}}, "imaps", {"jk": "<esc>"},
-#      "imaps dict not loaded properly with one entry"),
-#     ({"imaps": {"jk": "<esc>", "~": "/home/user"}},
-#      "imaps", {"jk": "<esc>", "~": "/home/user"},
-#      "imaps dict not loaded properly with two entries"),
-#     ({"imaps": {"jk": "<esc>", "~": "/home/user"},
-#       "load_configs": ["trash", "path"]},
-#      "imaps", {"jk": "<esc>", "~": "/home/user"},
-#      "imaps dict not loaded properly with two entries and multiple options"),
-#     ({"imaps": {"jk": "<esc>", "~": "/home/user"},
-#       "load_configs": ["trash", "path"]},
-#      "load_configs", ["trash", "path"],
-#      "load_configs dict not loaded properly with two entries and multiple options"),
-# ])
-# def test_config(caplog, tmpdir, conf, test_opt, expected, error_msg):
-#     caplog.set_level(logging.DEBUG)
-#     config.configStore = conf
-#     assert getConfigOption(test_opt) == expected, error_msg
+    adapter_maps.loadAdapterMaps("")
 
-# def test_additional_config(caplog, tmpdir):
-#     import json
-#     caplog.set_level(logging.DEBUG)
-#     with unittest.mock.patch('univisal.adapter_maps.getMappingPath',
-#                              return_value=tmpdir / "univisal" /
-#                              "mappings.json"):
-#         test = {"imaps": {"vk": "<esc>"}}
-#         expected = {"vk": "<esc>"}
-#         error_msg = "Additional config not loaded properly"
-#         init_config()
-#     assert getConfigOption("imaps") == expected, error_msg
-
-# @pytest.mark.parametrize("mapConfEntry, error_msg", [
-#     ({"imaps": {"jk": "<esc>"}}, "configured imaps did not expand"),
-#     ({"nmaps": {"j": "<left>"}}, "configured nmaps did not expand"),
-#     ({"nmaps": {"j": "<left>", "k": "<right>"}},
-#     "configured nmaps did not expand when multiple are configured"),
-# ])
-# def test_config_setMaps(mapConfEntry, error_msg):
-#     config.configStore = mapConfEntry
-#     config.setMaps()
-#     for mapModeType, maps in mapConfEntry.items():
-#         setCorrectModeForMap(mapModeType)
-#         assertMapsExpandCorrectly(maps, error_msg)
-
-# def setCorrectModeForMap(mapModeType):
-#     mode = getMapMode(mapModeType)
-#     model.setMode(mode)
-
-# def assertMapsExpandCorrectly(maps, error_msg):
-#     for sequence, expansion in maps.items():
-#         result = expandMap(sequence)
-#         assert result == [expansion], error_msg
-
-# def expandMap(sequence):
-#     for char in sequence:
-#         # Deliberately overwrite, we only want the last result (the
-#         # expansion).
-#         result = remap.resolve_map(char)
-#     removeBackspaces(result)
-#     return result
-
-# def removeBackspaces(lst):
-#     bs = Keys.backspace.value
-#     lst.remove(bs) if (bs in lst) else ''
-
-
-# @pytest.mark.parametrize("level, expected", [
-#     ("debug", logging.DEBUG),
-#     ("info", logging.INFO),
-#     ("error", logging.ERROR),
-#     ("warning", logging.WARNING),
-#     ("critical", logging.CRITICAL),
-# ])
-# def test_log_level(level, expected):
-#     import logging
-#     config.configStore={"log_level": level}
-#     setLogLevel()
-#     result = logging.getLogger().getEffectiveLevel()
-#     assert result == expected, \
-#         "Setting log level '{}' doesn't change effective level (is {})".format(
-#             level, result)
-
-
-
-# @pytest.mark.parametrize("opt, expected, msg", [
-#     (False, "m", "Unused normal keys are swallowed when they should not be"),
-#     (True, "", "Unused normal keys are not swallowed when they should be"),
-#     ])
-# def test_swallow_unused_normal(opt, expected, msg):
-#     from tests.mock_setup import init_univisal
-#     from univisal.handleInput import handleInput
-#     from univisal.handleKey import handleVimInputKey
-
-#     init_univisal()
-#     config.configStore={"swallow_unused_normal_keys": opt}
-#     setMode(Mode.normal)
-#     # Test handling the single key, then the whole input to confirm.
-#     result = handleVimInputKey("m")
-#     assert result == expected, msg + " after handleSingleInputKey"
-#     result = handleInput("m")
-#     assert result == expected, msg + " after handleInput"
+    assert adapter_maps.adapter_maps == mock_maps, \
+        "Adapter maps don't load correctly"
+    assert adapter_maps.getAdapterMap(key) == value, \
+        "Loaded adapter maps don't provide values for lookup"
